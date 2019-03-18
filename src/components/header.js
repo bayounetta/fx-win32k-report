0b4e74b327704b633549436f08944432c2201c6b
juniper wilde <juniper.r.wilde@gmail.com>
2019-03-18 10:04:49 -0700

    add header and css

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import headerStyles from './header.module.css';
+
 export default (props) => (
-  <div>
+  <div className={headerStyles.header}>
     <p>Firefox win32k reports!{props.title}</p>
   </div>
 );
