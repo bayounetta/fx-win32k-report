7365c82e1b354fced8f148cdee192bf28353b5a4
juniper wilde <juniper.r.wilde@gmail.com>
2019-03-18 09:22:24 -0700

    TXT to JSON conversion script

---
+++ src/utility/deploy.js
@@ -0,0 +1,15 @@
+const ghpages = require('gh-pages');
+
+ghpages.publish(
+    'public', {
+      branch: 'gh-pages',
+      repo: 'https://github.com/metalcanine/fx-win32k-report.git',
+    },
+    (err) => {
+      if (!err) {
+        console.log('deployed!');
+      }
+      else {
+        console.error(err);
+      }
+    })
