052ea7d9faeb96ed60413a3be8f45abdc9351292
juniper wilde <juniper.r.wilde@gmail.com>
2019-03-25 15:09:36 -0700

    add page links to header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,14 @@
+import { Link } from 'gatsby';
 import React from 'react';
 
 import headerStyles from './header.module.css';
 
 export default (props) => (
   <div className={headerStyles.header}>
-    <p>Firefox win32k reports!{props.title}</p>
+    <div className={headerStyles.title}>Firefox win32k reports! {props.title}</div>
+    <div className={headerStyles.navigation}>
+      <a className={headerStyles.navigationLink} href="/">index</a>
+      <a className={headerStyles.navigationLink} href="/about">about</a>
+    </div>
   </div>
 );
