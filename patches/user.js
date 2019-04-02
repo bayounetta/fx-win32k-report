2aff66d6f9c4df054c6ada884f495fd674c69162
juniper wilde <juniper.r.wilde@gmail.com>
2019-04-02 09:15:37 -0700

    Add initial verisons of local patches to import and pref file

---
+++ patches/user.js
@@ -0,0 +1,7 @@
+// user_pref("media.cubeb.sandbox", false);
+user_pref("gfx.canvas.remote", true);
+user_pref("webgl.disabled", true);
+user_pref("mozilla.widget.disable-native-theme", true);
+user_pref("gfx.webrender.enabled", true);
+user_pref("browser.preferences.defaultPerformanceSettings.enabled", false);
+user_pref("dom.ipc.processCount", 1);
