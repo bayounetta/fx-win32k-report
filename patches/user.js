0a465b094e6a5d77bad29eb901788b2efbaf680b
juniper wilde <juniper.r.wilde@gmail.com>
2019-04-12 07:15:10 -0700

    update prefs to include autoplay videos and cubeb remote

--- patches/user.js
+++ patches/user.js
@@ -1,7 +1,8 @@
-// user_pref("media.cubeb.sandbox", false);
+user_pref("media.cubeb.sandbox", true);
 user_pref("gfx.canvas.remote", true);
 user_pref("webgl.disabled", true);
 user_pref("mozilla.widget.disable-native-theme", true);
 user_pref("gfx.webrender.enabled", true);
 user_pref("browser.preferences.defaultPerformanceSettings.enabled", false);
 user_pref("dom.ipc.processCount", 1);
+user_pref("media.autoplay.default", 0);
