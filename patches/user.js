79fe3942ed9c7b03205d39e81e470f6393b33d59
juniper wilde <juniper.r.wilde@gmail.com>
2019-04-15 11:44:09 -0700

    Add dom.ipc.useNativeEventProcessing.content to prefs

--- patches/user.js
+++ patches/user.js
@@ -1,4 +1,4 @@
-user_pref("media.cubeb.sandbox", true);
+// user_pref("media.cubeb.sandbox", true);
 user_pref("gfx.canvas.remote", true);
 user_pref("webgl.disabled", true);
 user_pref("mozilla.widget.disable-native-theme", true);
@@ -6,3 +6,4 @@ user_pref("gfx.webrender.enabled", true);
 user_pref("browser.preferences.defaultPerformanceSettings.enabled", false);
 user_pref("dom.ipc.processCount", 1);
 user_pref("media.autoplay.default", 0);
+user_pref("dom.ipc.useNativeEventProcessing.content", true);
